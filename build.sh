270dc9f9442c8c0047a1e59b5f54d596194dd8c1
murashovzhen <murashov_zhen@mail.ru>
2018-03-02 07:57:37 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-dotnet restore ./serilog-sinks-syslog.sln --runtime netstandard2.0
+dotnet restore ./serilog-sinks-syslog.sln --runtime netstandard1.6
 #dotnet build ./src/Serilog.Sinks.Syslog/Serilog.Sinks.Syslog.csproj --runtime netstandard2.0 --configuration Release
 
 dotnet build ./test/Serilog.Sinks.Syslog.Tests/Serilog.Sinks.Syslog.Tests.csproj --framework netcoreapp2.0
